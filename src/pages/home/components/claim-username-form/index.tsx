7e7b795bf6b9143fd0a08c0c2a24bd8d5a04a3b3
nikollllllas <nikollasohta2014@gmail.com>
2024-05-23 09:43:28 -0300

    feat: username persistence to register in url

--- src/pages/home/components/claim-username-form/index.tsx
+++ src/pages/home/components/claim-username-form/index.tsx
@@ -4,51 +4,66 @@ import { useForm } from 'react-hook-form'
 import { Form, FormAnnotation } from './styles'
 import z from 'zod'
 import { zodResolver } from '@hookform/resolvers/zod'
-
+import { useRouter } from 'next/router'
 
 const claimUsernameFormSchema = z.object({
   username: z
-  .string()
-  .min(3, { message: 'O usuário deve ter no mínimo 3 caracteres' })
-  .max(20, { message: 'O usuário deve ter no máximo 20 caracteres' })
-  .regex(/^([a-z\\-]+)$/i, { message: 'O usuário deve ter no apenas letras e hífens' })
-  .transform(username => username.toLowerCase())
+    .string()
+    .min(3, { message: 'O usuário deve ter no mínimo 3 caracteres' })
+    .max(20, { message: 'O usuário deve ter no máximo 20 caracteres' })
+    .regex(/^([a-z\\-]+)$/i, {
+      message: 'O usuário deve ter no apenas letras e hífens',
+    })
+    .transform((username) => username.toLowerCase()),
 })
-
 
 type ClaimUsernameFormData = z.infer<typeof claimUsernameFormSchema>
 
 export function ClaimUsernameForm() {
-  const { register, handleSubmit, formState: { errors } } = useForm<ClaimUsernameFormData>({
-    resolver: zodResolver(claimUsernameFormSchema)
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitting },
+  } = useForm<ClaimUsernameFormData>({
+    resolver: zodResolver(claimUsernameFormSchema),
   })
 
-  async function handleClaimUsername(data: ClaimUsernameFormData) {
+  const router = useRouter()
 
+  async function handleClaimUsername(data: ClaimUsernameFormData) {
+    const { username } = data
+
+    await router.push(`/register?username=${username}`)
   }
 
   return (
     <>
-    <Form as='form' onSubmit={handleSubmit(handleClaimUsername)}>
-      <TextInput
-        size='sm'
-        prefix='ignite.com/'
-        placeholder='seu-usuario'
-        {...register('username')}
-      />
+      <Form
+        as='form'
+        onSubmit={handleSubmit(handleClaimUsername)}>
+        <TextInput
+          size='sm'
+          prefix='ignite.com/'
+          placeholder='seu-usuario'
+          {...register('username')}
+        />
 
-      <Button
-        size='sm'
-        type='submit'>
-        Reservar
-        <ArrowRight />
-      </Button>
+        <Button
+          size='sm'
+          type='submit'
+          disabled={isSubmitting}>
+          Reservar
+          <ArrowRight />
+        </Button>
+      </Form>
 
-    </Form>
       <FormAnnotation>
         <Text size='sm'>
-          { errors.username ? errors.username.message : 'Digite o nome do usuário desejado'}
+          {errors.username
+            ? errors.username.message
+            : 'Digite o nome do usuário desejado'}
         </Text>
-      </FormAnnotation></>
+      </FormAnnotation>
+    </>
   )
 }
--- src/pages/register/index.page.tsx
+++ src/pages/register/index.page.tsx
@@ -4,6 +4,8 @@ import { ArrowRight } from '@phosphor-icons/react/dist/icons/ArrowRight'
 import { useForm } from 'react-hook-form'
 import z from 'zod'
 import { zodResolver } from '@hookform/resolvers/zod'
+import { useRouter } from 'next/router'
+import { useEffect } from 'react'
 
 const registerFormSchema = z.object({
   username: z
@@ -23,11 +25,20 @@ type RegisterFormData = z.infer<typeof registerFormSchema>
 export default function Register() {
   const {
     register,
+    setValue,
     handleSubmit,
     formState: { errors, isSubmitting },
   } = useForm<RegisterFormData>({
     resolver: zodResolver(registerFormSchema),
   })
+
+  const router = useRouter()
+
+  useEffect(() => {
+    if (router.query.username) {
+      setValue('username', String(router.query.username))
+    }
+  }, [router.query?.username, setValue])
 
   async function handleRegister(data: RegisterFormData) {
     console.log(data)
